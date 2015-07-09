820b74e1deabbc4140419bb22460bc0c27b35692
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-09 19:18:30 +0100

    fixing temperature so it's not fixed, adjusts based on location

--- src/javascripts/location.js
+++ src/javascripts/location.js
@@ -2,6 +2,7 @@ var latitude;
 var longitude;
 
 navigator.geolocation.getCurrentPosition(GetLocation);
+
 function GetLocation(location) {
     latitude = (location.coords.latitude);
     longitude = (location.coords.longitude);
@@ -9,8 +10,23 @@ function GetLocation(location) {
 }
 
 function getAddress(latitude, longitude) {
-  geocoder = new google.maps.Geocoder();
+  var geocoder = new google.maps.Geocoder();
   geocoder.geocode({'latLng': new google.maps.LatLng(latitude, longitude) }, function(results, status) {
-    $('span#location').html(results[0].formatted_address);
+    var locationData = results[0].address_components;
+    for (var i = 0; i < locationData.length; i++ ){
+        if (locationData[i].types[0] === "route") { var address = locationData[i].long_name};
+        if (locationData[i].types[0] === "postal_town") { var town = locationData[i].long_name};
+      };
+      console.log(address);
+      console.log(town);
+    $('span#location').html(address);
+    getTemperature(town);
   });
+}
+
+function getTemperature(location){
+  var request = 'http://api.openweathermap.org/data/2.5/weather?q=' + location;
+  $.getJSON(request, function(data) {
+    $('#locationTemp').html(Math.round(data.main.temp - 273.15).toString());
+    });
 }
